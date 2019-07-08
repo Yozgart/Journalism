633df28f3be0d876a962af5dbad55ce6d9600d8c
Yozgart <yozgart@gmail.com>
2019-07-07 20:01:05 -0500

    Version

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,149 +1,151 @@
+// Set up chart
 var svgWidth = 960;
 var svgHeight = 500;
-
-var margin = {
-  top: 20,
-  right: 40,
-  bottom: 60,
-  left: 100
-};
-
+var margin = {top: 20, right: 40, bottom: 60, left: 100};
 var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
 // Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
-var svg = d3.select("body")
-  .append("svg")
-  .attr("width", svgWidth)
-  .attr("height", svgHeight);
+var svg = d3
+  .select('.chart')
+  .append('svg')
+  .attr('width', svgWidth)
+  .attr('height', svgHeight)
+  .append('g')
+  .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+var chart = svg.append('g');
 
-var chartGroup = svg.append("g")
-  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+// Append a div to the body to create tooltips, assign it a class
+d3.select(".chart").append("div").attr("class", "tooltip").style("opacity", 0);
 
-d3.select("body").append("div").attr("class", "tooltip").style("opacity", 0);
+// Retrieve data from CSV file and execute everything below
+d3.csv("../../data/data.csv", function(err, healthData) {
+    if(err) throw err;
 
-// Import Data
-d3.csv("data.csv", function(err, healthData) {
-  if (err) throw err;
-console.log(healthData)
-  // Step 1: Parse Data/Cast as numbers
-   // ==============================
-  healthData.forEach(function(data) {
-    data.poverty = +data.poverty;
-    data.healthcare = +data.healthcare;
-  });
+    healthData.forEach(function(data) {
+        data.poverty = +data.poverty;
+        data.phys_act = +data.phys_act;
+    });
+    
+    // Create scale functions
+    var yLinearScale = d3.scaleLinear().range([height, 0]);
+    var xLinearScale = d3.scaleLinear().range([0, width]);
 
-  // Step 2: Create scale functions
-  // ==============================
-  var xLinearScale = d3.scaleLinear().range([0, width]);
-  var yLinearScale = d3.scaleLinear().range([height, 0]);
+    // Create axis functions
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
 
-  // Step 3: Create axis functions
-  // ==============================
-  var bottomAxis = d3.axisBottom(xLinearScale);
-  var leftAxis = d3.axisLeft(yLinearScale);
+    // Scale the domain
+    var xMin;
+    var xMax;
+    var yMin;
+    var yMax;
 
-  var xMin;
-  var xMax;
-  var yMin;
-  var yMax;
-  
-  xMin = d3.min(healthData, function(data) {
-      return data.healthcare;
-  });
-  
-  xMax = d3.max(healthData, function(data) {
-      return data.healthcare;
-  });
-  
-  yMin = d3.min(healthData, function(data) {
-      return data.poverty;
-  });
-  
-  yMax = d3.max(healthData, function(data) {
-      return data.poverty;
-  });
-  
-  xLinearScale.domain([xMin, xMax]);
-  yLinearScale.domain([yMin, yMax]);
-  console.log(xMin);
-  console.log(yMax);
-
-  // Step 4: Append Axes to the chart
-  // ==============================
-  chartGroup.append("g")
-    .attr("transform", `translate(0, ${height})`)
-    .call(bottomAxis);
-
-  chartGroup.append("g")
-    .call(leftAxis);
-
-   // Step 5: Create Circles
-  // ==============================
-  var circlesGroup = chartGroup.selectAll("circle")
-  .data(healthData)
-  .enter()
-  .append("circle")
-  .attr("cx", d => xLinearScale(d.healthcare +1.5))
-  .attr("cy", d => yLinearScale(d.poverty +0.3))
-  .attr("r", "12")
-  .attr("fill", "blue")
-  .attr("opacity", .5)
-
-  .on("mouseout", function(data, index) {
-    toolTip.hide(data);
-  });
-  // Step 6: Initialize tool tip
-  // ==============================
-  var toolTip = d3.tip()
-    .attr("class", "tooltip")
-    .offset([80, -60])
-    .html(function(d) {
-      return (abbr + '%');
-      });
-
-  // Step 7: Create tooltip in the chart
-  // ==============================
-  chartGroup.call(toolTip);
-
-  // Step 8: Create event listeners to display and hide the tooltip
-  // ==============================
-  circlesGroup.on("click", function(data) {
-    toolTip.show(data);
-  })
-    // onmouseout event
-    .on("mouseout", function(data, index) {
-      toolTip.hide(data);
+    xMin = d3.min(healthData, function(data) {
+        return +data.poverty * 0.95;
     });
 
-  // Create axes labels
+    xMax = d3.max(healthData, function(data) {
+        return +data.poverty * 1.05;
+    });
 
-  chartGroup.append("text")
-  .style("font-size", "12px")
-  .selectAll("tspan")
-  .data(healthData)
-  .enter()
-  .append("tspan")
-      .attr("x", function(data) {
-          return xLinearScale(data.healthcare +1.3);
-      })
-      .attr("y", function(data) {
-          return yLinearScale(data.poverty +.1);
-      })
-      .text(function(data) {
-          return data.abbr
-      });
+    yMin = d3.min(healthData, function(data) {
+        return +data.phys_act * 0.98;
+    });
 
-  chartGroup.append("text")
-    .attr("transform", "rotate(-90)")
-    .attr("y", 0 - margin.left + 40)
-    .attr("x", 0 - (height / 2))
-    .attr("dy", "1em")
-    .attr("class", "axisText")
-    .text("Lacks Healtcare(%)");
+    yMax = d3.max(healthData, function(data) {
+        return +data.phys_act *1.02;
+    });
+    
+    xLinearScale.domain([xMin, xMax]);
+    yLinearScale.domain([yMin, yMax]);
 
-  chartGroup.append("g")
-    .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
-    .attr("class", "axisText")
-    .text("In Poverty (%)");
-});
+    
+    // Initialize tooltip 
+    var toolTip = d3
+        .tip()
+        .attr("class", "tooltip")
+        .offset([80, -60])
+        .html(function(data) {
+            var stateName = data.state;
+            var pov = +data.poverty;
+            var physAct = +data.phys_act;
+            return (
+                stateName + '<br> Poverty: ' + pov + '% <br> Physically Active: ' + physAct +'%'
+            );
+        });
+
+    // Create tooltip
+    chart.call(toolTip);
+
+    chart.selectAll("circle")
+        .data(healthData)
+        .enter()
+        .append("circle")
+        .attr("cx", function(data, index) {
+            return xLinearScale(data.poverty)
+        })
+        .attr("cy", function(data, index) {
+            return yLinearScale(data.phys_act)
+        })
+        .attr("r", "15")
+        .attr("fill", "lightblue")
+        // display tooltip on click
+        .on("mouseenter", function(data) {
+            toolTip.show(data);
+        })
+        // hide tooltip on mouseout
+        .on("mouseout", function(data, index) {
+            toolTip.hide(data);
+        });
+    
+    // Appending a label to each data point
+    chart.append("text")
+        .style("text-anchor", "middle")
+        .style("font-size", "12px")
+        .selectAll("tspan")
+        .data(healthData)
+        .enter()
+        .append("tspan")
+            .attr("x", function(data) {
+                return xLinearScale(data.poverty - 0);
+            })
+            .attr("y", function(data) {
+                return yLinearScale(data.phys_act - 0.2);
+            })
+            .text(function(data) {
+                return data.abbr
+            });
+    
+    // Append an SVG group for the xaxis, then display x-axis 
+    chart
+        .append("g")
+        .attr('transform', `translate(0, ${height})`)
+        .call(bottomAxis);
+
+    // Append a group for y-axis, then display it
+    chart.append("g").call(leftAxis);
+
+    // Append y-axis label
+    chart
+        .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0-margin.left + 40)
+        .attr("x", 0 - height/2)
+        .attr("dy","1em")
+        .attr("class", "axis-text")
+        .text("Physically Active (%)")
+
+    // Append x-axis labels
+    chart
+        .append("text")
+        .attr(
+            "transform",
+            "translate(" + width / 2 + " ," + (height + margin.top + 30) + ")"
+        )
+        .attr("class", "axis-text")
+        .text("In Poverty (%)");
+    });
+
+
+
